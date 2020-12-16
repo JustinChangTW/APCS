efb0592259064406266fab3427217ebdd6ca09d0
justin <justin.chang.tw@outlook.com>
2020-12-16 17:36:31 +0800

    簡單輸出入

---
+++ training/inputAndOutput/simple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    //輸出
+    cout << "哈囉";
+
+    //輸入
+    int x;
+    cin >> x;
+    cout << x << endl;
+
+
+    //連續輸入
+    int a,b,c,d;
+    cin >> a >> b >> c  >> d;
+    cout << a + b + c + d << endl;
+
+    return 0;
+}
